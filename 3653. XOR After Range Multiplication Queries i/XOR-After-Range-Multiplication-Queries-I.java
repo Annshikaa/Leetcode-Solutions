e71f92746bb2bae2d2390f913b3cb690b868c040
Annshikaa <163762779+Annshikaa@users.noreply.github.com>
2026-04-08 09:27:11 +0530

    Update XOR-After-Range-Multiplication-Queries-I.java

--- 3653. XOR After Range Multiplication Queries i/XOR-After-Range-Multiplication-Queries-I.java
+++ 3653. XOR After Range Multiplication Queries i/XOR-After-Range-Multiplication-Queries-I.java
@@ -1,1 +1,27 @@
+class Solution {
+    public int xorAfterQueries(int[] nums, int[][] queries) {
+        int MOD = 1000000007;
 
+        // Apply each query one by one
+        for (int i = 0; i < queries.length; i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
+            int k = queries[i][2];
+            int v = queries[i][3];
+
+            // Move from l to r with step k
+            for (int idx = l; idx <= r; idx += k) {
+                long updated = (long) nums[idx] * v;
+                nums[idx] = (int) (updated % MOD);
+            }
+        }
+
+        // Calculate XOR of final array
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+
+        return result;
+    }
+}
