ef1c651dcc3935f3ec3ab4167327febfb69065e9
Anshika Jain <163762779+Annshikaa@users.noreply.github.com>
2026-06-11 20:53:05 +0530

    Time: 0 ms (100%), Space: 43.5 MB (58.43%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int l=0, r=nums.length-1, i=0;
+        while(i<=r){
+            if(nums[i] == 0){
+                nums[i++] = nums[l];
+                nums[l++] = 0;
+            }
+            else if(nums[i] == 2){
+                nums[i] = nums[r];
+                nums[r--] = 2;
+            }
+            else i++;
+        }
+    }
+}
